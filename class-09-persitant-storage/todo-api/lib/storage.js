755a2eb1274509ecdbc91801b9d51264404e01b0
Duncan Marsh <dwolfm@gmail.com>
2017-01-12 12:14:01 -0800

    updated storage module

--- class-09-persitant-storage/todo-api/lib/storage.js
+++ class-09-persitant-storage/todo-api/lib/storage.js
@@ -3,52 +3,77 @@
 
 
 // data =
-// { 
+// {
 // notes: {123: {}, 534: {}},
 // list: {120: {}, 530: {}}
 // }
 
 //./data/
-   //| ./notes/
-   //|   123 -- JSON File with hte 123 note
-   //|   534 -- JSON File with hte 534 note
-   //| ./list/
-   //|   120 -- JSON File with hte 120 note
-   //|   530 -- JSON File with hte 530 note
+//| ./notes/
+//|   123.json -- JSON File with hte 123 note
+//|   534.json -- JSON File with hte 534 note
+//| ./list/
+//|   120.json -- JSON File with hte 120 note
+//|   530.json -- JSON File with hte 530 note
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 
 const data = {};
 const storage = module.exports = {};
 
-storage.setItem = function(name, item){
-  if (!data[name]) data[name] = {};
-  data[name][item.id] = item;
+const dataDir = `${__dirname}/../data`;
 
-  // on success resolve the item that was stored
-  return Promise.resolve(item);
+//storage.setItem('notes', {id: 123, title: 'hello')
+//.then(note => {
+//})
+
+storage.setItem = function(name, item){
+  // TODO: check if directory exits
+  return  fs.statAsync(`${dataDir}/${name}`)
+  .catch(err => {
+    err.status = 400;
+    return Promise.reject(err);
+  })
+  .then(() => {
+    let json = JSON.stringify(item);
+    // if this fails its 500 error
+    // TODO: save the item in the directory
+    return fs.writeFileAsync(`${dataDir}/${name}/${item.id}.json`, json);
+  })
+  // TODO: respond to the user
+  .then(() => item);
+
 }
 
 storage.getItem = function(name, id){
-  if (!data[name] || !data[name][id]) {
-    let err = new Error('item not found');
-    err.status = 404;
+  // TODO check if name dir xists if not 404
+  // TODO check if JSON file with id name exists if not 404
+  // TODO: read json file parse and send back
+  return fs.statAsync(`${dataDir}/${name}/${id}.json`)
+  .catch(err => {
     return Promise.reject(err);
-  }
+    err.status = 404;
+  })
+  .then(() => {
+    return fs.readFileAsync(`${dataDir}/${name}/${id}.json`)
+  })
+  .then(data => {
+    return JSON.parse(data.toString());
+  })
 
-  return Promise.resolve(data[name][id]);
 }
 
 storage.deleteItem = function(name, id){
+  // TODO: use fs.statAsync to check if file exts
+  // if not in catch blcok make the error a 404 and reject it
   if (!data[name] || !data[name][id]) {
     let err = new Error('item not found');
     err.status = 404;
     return Promise.reject(err);
   }
-
+  
   delete data[name][id];
   return Promise.resolve();
 }
 
-//storage.setItem('notes', {id: 'lulwat' ...})
 
